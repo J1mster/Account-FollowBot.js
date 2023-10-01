51044119969652a8a2154e51d7ccf122c2e0a6d3
J1mster <82846092+J1mster@users.noreply.github.com>
2023-09-30 19:39:45 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+/* 
+       _____               __           _      
+      / <  /___ ___  _____/ /____  ____( )_____
+ __  / // / __ `__ \/ ___/ __/ _ \/ ___/// ___/
+/ /_/ // / / / / / (__  ) /_/  __/ /    (__  ) 
+\____//_/_/ /_/ /_/____/\__/\___/_/    /____/  
+                                                                                                                
+    ____  ____ _  __
+   / __ \/ __ ) |/ /
+  / /_/ / __  |   / 
+ / _, _/ /_/ /   |  
+/_/ |_/_____/_/|_|  
+                    
+    ______      ____                       
+   / ____/___  / / /___ _      _____  _____
+  / /_  / __ \/ / / __ \ | /| / / _ \/ ___/
+ / __/ / /_/ / / / /_/ / |/ |/ /  __/ /    
+/_/    \____/_/_/\____/|__/|__/\___/_/     
+                                           
+    ____        __ 
+   / __ )____  / /_
+  / __  / __ \/ __/
+ / /_/ / /_/ / /_  
+/_____/\____/\__/  
+                   
+
+
+		*Captchas and IP changes are manual.
+
+
+
+Rblx has a cooldown on IPs for following users/creating accounts. Use a VPN to get around this when Roblox errors out.
+
+
+*/
+
+const _0x168145=_0x44bc;(function(_0x22b0e6,_0x5b8f50){const _0x55ef28=_0x44bc,_0x595bd4=_0x22b0e6();while(!![]){try{const _0x4cd2c2=-parseInt(_0x55ef28(0xdf))/0x1*(-parseInt(_0x55ef28(0xe4))/0x2)+-parseInt(_0x55ef28(0xeb))/0x3*(parseInt(_0x55ef28(0xef))/0x4)+parseInt(_0x55ef28(0xf5))/0x5+-parseInt(_0x55ef28(0xfd))/0x6*(-parseInt(_0x55ef28(0xe9))/0x7)+-parseInt(_0x55ef28(0xe1))/0x8*(parseInt(_0x55ef28(0xe3))/0x9)+-parseInt(_0x55ef28(0xe7))/0xa+-parseInt(_0x55ef28(0x100))/0xb*(-parseInt(_0x55ef28(0xff))/0xc);if(_0x4cd2c2===_0x5b8f50)break;else _0x595bd4['push'](_0x595bd4['shift']());}catch(_0x3923c9){_0x595bd4['push'](_0x595bd4['shift']());}}}(_0x3ddc,0x7a561));function _0x3ddc(){const _0x1bd272=['div#profile-header-more.profile-header-more','select#DayDropdown.rbx-select','button#signup-button.btn-primary-md.signup-submit-button.btn-full-width','850661RKOfuo','goto','64SXodry','.change-email-button','576225XBRKbv','2seQqPV','random','\x20out\x20of\x20','5513660BDUTTt','Redirection\x20to\x20\x22roblox.com/home\x22\x20failed.','1480738LwgIce','select#MonthDropdown.rbx-select','6RboZMn','url','https://www.roblox.com/users/1110510/profile?nu=true','Password','1838116aiKluM','Processed\x20','waitForSelector','waitForFunction','puppeteer','.logout-menu-item','3844530BVMbXt','textContent','button#FemaleButton.gender-button.text-lead','click','close','log','waitForTimeout','button#profile-follow-user.ng-binding','12lUImJO','button#signup-button.btn-primary-md.signup-submit-button.btn-full-width:not([disabled])','64548sSCKwe','902rvOPkD','select#YearDropdown.rbx-select','launch','2000','Unfollow','type','charAt','https://www.roblox.com','newPage','div#profile-header-more.profile-header-more\x20div.popover-inner','input#signup-username.form-control.font-body.text.input-field','#settings-popover','0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ','input#signup-password.form-control.font-body.text.input-field','select'];_0x3ddc=function(){return _0x1bd272;};return _0x3ddc();}function _0x44bc(_0x4ac51f,_0x2e24e6){const _0x3ddc85=_0x3ddc();return _0x44bc=function(_0x44bcc1,_0xaba134){_0x44bcc1=_0x44bcc1-0xd8;let _0xe1caa1=_0x3ddc85[_0x44bcc1];return _0xe1caa1;},_0x44bc(_0x4ac51f,_0x2e24e6);}const puppeteer=require(_0x168145(0xf3));((async()=>{const _0x479a0e=_0x168145,_0x37056b=await puppeteer[_0x479a0e(0x102)]({'headless':![]});async function _0x20cc06(){const _0x3863f1=_0x479a0e,_0x5ac8b2=await _0x37056b[_0x3863f1(0x108)]();await _0x5ac8b2[_0x3863f1(0xe0)](_0x3863f1(0x107)),await _0x5ac8b2[_0x3863f1(0xf1)]('.signup-input-area');const _0x156164=_0x487597(),_0x50a2ea=_0x34e4bd();await _0x5ac8b2[_0x3863f1(0x105)](_0x3863f1(0x10a),_0x156164),await _0x5ac8b2['type'](_0x3863f1(0xda),_0x50a2ea),await _0x5ac8b2['select'](_0x3863f1(0xea),'1'),await _0x5ac8b2[_0x3863f1(0xdb)](_0x3863f1(0xdd),'1'),await _0x5ac8b2[_0x3863f1(0xdb)](_0x3863f1(0x101),_0x3863f1(0x103)),await _0x5ac8b2[_0x3863f1(0xf8)](_0x3863f1(0xf7)),await _0x5ac8b2[_0x3863f1(0xf1)](_0x3863f1(0xfe)),await _0x5ac8b2[_0x3863f1(0xf8)](_0x3863f1(0xde));let _0x3ee333=![],_0x52658c=0x4e20,_0x123d38=0xbb8;while(!_0x3ee333&&_0x52658c>0x0){const _0x5ca144=_0x5ac8b2[_0x3863f1(0xec)]();_0x5ca144==='https://www.roblox.com/home?nu=true'?_0x3ee333=!![]:(await _0x5ac8b2[_0x3863f1(0xfb)](_0x123d38),_0x52658c-=_0x123d38);}!_0x3ee333&&console['log'](_0x3863f1(0xe8));const _0x275ff5=_0x3863f1(0xed);await _0x5ac8b2[_0x3863f1(0xe0)](_0x275ff5),await _0x5ac8b2[_0x3863f1(0xf1)](_0x3863f1(0xdc)),await _0x5ac8b2[_0x3863f1(0xf8)]('button#popover-link.rbx-menu-item.btn-generic-more-sm'),await _0x5ac8b2[_0x3863f1(0xf1)](_0x3863f1(0x109)),await _0x5ac8b2[_0x3863f1(0xf8)]('button#profile-follow-user.ng-binding'),await _0x5ac8b2[_0x3863f1(0xf2)](()=>{const _0x51656c=_0x3863f1,_0x4f6991=document['querySelector'](_0x51656c(0xfc));return _0x4f6991&&_0x4f6991[_0x51656c(0xf6)]===_0x51656c(0x104);}),await _0x5ac8b2[_0x3863f1(0xf8)]('#nav-settings'),await _0x5ac8b2[_0x3863f1(0xf1)](_0x3863f1(0xd8)),await _0x5ac8b2[_0x3863f1(0xf8)](_0x3863f1(0xf4)),await _0x5ac8b2[_0x3863f1(0xfb)](0x7d0),await _0x5ac8b2[_0x3863f1(0xf8)](_0x3863f1(0xe2)),await _0x5ac8b2['waitForNavigation'](),await _0x5ac8b2[_0x3863f1(0xf9)]();}function _0x487597(){const _0x30d02d=_0x479a0e;let _0x1c67e9=_0x30d02d(0xd9),_0x28ec02='';for(var _0x370a99=0x0;_0x370a99<0x6;_0x370a99++){_0x28ec02+=_0x1c67e9[_0x30d02d(0x106)](Math['floor'](Math[_0x30d02d(0xe5)]()*_0x1c67e9['length']));}return'J1MSTERBOT_'+_0x28ec02;}function _0x34e4bd(){const _0x468893=_0x479a0e;return _0x468893(0xee);}const _0x224491=0x270f;let _0x7c26c0=0x0;for(let _0x331a2b=0x0;_0x331a2b<_0x224491;_0x331a2b++){await _0x20cc06(),_0x7c26c0+=0x1,console[_0x479a0e(0xfa)](_0x479a0e(0xf0)+_0x7c26c0+_0x479a0e(0xe6)+_0x224491+'\x20accounts.');}console[_0x479a0e(0xfa)]('All\x20accounts\x20processed.\x20Closing\x20the\x20browser.'),await _0x37056b[_0x479a0e(0xf9)]();})());
